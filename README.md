37557de263cf633f4f81cea2cd1a7ad59de10ffe
intissarMez <intissar.elmezrouai@gmail.com>
2016-11-11 09:28:17 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# ng2-coding-dojo
+
---
+++ app/app.component.ts
@@ -0,0 +1,6 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'my-app',
+  template: '<h1>My Angular 2 App is alive !!</h1>'
+})
+export class AppComponent { }
